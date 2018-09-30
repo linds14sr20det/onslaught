6ab2bf651e29fc841ee34db1c3132a175c63029e
Stephen Lind <stephen.l@getjobber.com>
2018-09-30 17:43:02 -0600

    round navigation working

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -2,6 +2,8 @@ class SystemsController < ApplicationController
   before_action :logged_in_user
 
   def show
+    @round_number = params[:round].to_i
+    @pairings = []
     @system = System.find(params[:id])
     @registrants = @system.registrants.paid
   end
