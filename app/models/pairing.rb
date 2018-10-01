e553b8100e9fd42f12f3c9a0110269c44fb6a246
KyleJastrick <jastrick.mazziki@gmail.com>
2018-09-30 18:48:33 -0600

    populate pairing model

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -1,3 +1,6 @@
 class Pairing < ApplicationRecord
-
+  belongs_to :system, inverse_of: :pairings
+  has_one :player_one, inverse_of: :pairing
+  has_one :player_two, inverse_of: :pairing
+  validates :round, presence: true
 end
