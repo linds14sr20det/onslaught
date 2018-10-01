8e66df69fc399d2d54910525cd70bc04a13ce834
KyleJastrick <jastrick.mazziki@gmail.com>
2018-09-30 19:23:08 -0600

    add models for rounds

--- app/models/round_aggregate.rb
+++ app/models/round_aggregate.rb
@@ -1,3 +1,6 @@
 class RoundAggregate < ApplicationRecord
-
+	has_many :round_individual, inverse_of: :round_aggregate
+	has_one :player, inverse_of: :round_aggregate
+	has_one :opponent, inverse_of: :round_aggregate
+	has_one :system, inverse_of: :round_aggregate
 end
--- app/models/round_individual.rb
+++ app/models/round_individual.rb
@@ -1,3 +1,6 @@
 class RoundIndividual < ApplicationRecord
-
+	has_one :player, inverse_of: :round_individual
+	has_one :opponent, inverse_of: :round_individual
+	has_one :system, inverse_of: :round_individual
+	has_one :round_aggregate,  inverse_of: :round_individual
 end
